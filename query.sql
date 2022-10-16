df8171c26aa6c33b66e3dc5a53aeaaf8ff9451d7
Kuanysh <107105973+kuanyshbakytuly@users.noreply.github.com>
2022-10-16 18:10:17 +0600

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,13 @@
+--sorting ID via Left Exclusive Join
 select a.id 
 from krisha_id as a 
     left join krishaaa as b on a.id = b.id 
 where b.id is NULL order by 1;
+
+
+--sorting most expensive apartment
+--Цена - price
+--Город - city
+select Цена
+from krisha_id
+group by Город
